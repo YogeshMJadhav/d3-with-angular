19d8183bd4d4ce7bf4b519827eb22d48da18b559
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-09-18 13:25:15 +0530

    Add D3 graph component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { DataModel } from './data/data.model';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data: Observable<DataModel>;
+    title = 'D3.js with Angular!';
 
-  constructor(private http: HttpClient) {
-    this.data = this.http.get<DataModel>('assets/data.json');
-  }
+    examples = [
+        {
+            title: 'Line Chart',
+            route: '/line-chart'
+        },
+        {
+            title: 'Multi Series Line Chart',
+            route: '/multi-series'
+        },
+        {
+            title: 'Bar Chart',
+            route: '/bar-chart'
+        },
+        {
+            title: 'Stacked Bar Chart',
+            route: '/stacked-bar-chart'
+        },
+        {
+            title: 'Brush Zoom',
+            route: '/brush-zoom'
+        },
+        {
+            title: 'Pie Chart',
+            route: '/pie-chart'
+        },
+        {
+            title: 'Donut chart',
+            route: '/donut-chart'
+        },
+    ];
+
 }
